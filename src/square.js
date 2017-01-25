34819ed65f173615249c2daf9a240fc14d7f1411
J. Fritz Barnes <fritz@stonevalleypartners.com>
2017-01-25 14:56:46 -0800

    add rewire example

---
+++ src/square.js
@@ -0,0 +1,8 @@
+import {Calculator} from './calculator';
+
+export default class Square {
+  square(num) {
+    let c = new Calculator();
+    return c.multiply(num, num);
+  }
+}
---
+++ test/square-spec.js
@@ -0,0 +1,28 @@
+import Square from '../src/square';
+
+describe('Square', () => {
+
+ it('without rewiring', () => {
+   let square = new Square();
+   let sum = square.square(4);
+
+   expect(sum).toBe(16);
+ });
+
+ class MockCalculator {
+   multiply(num) {
+     return 'no one wants to be square';
+   }
+ }
+
+  it('rewired', () => {
+    Square.__Rewire__('Calculator', MockCalculator);
+
+    let square = new Square();
+    let snark = square.square(4);
+
+    expect(snark).toBe('no one wants to be square');
+  });
+
+});
+
